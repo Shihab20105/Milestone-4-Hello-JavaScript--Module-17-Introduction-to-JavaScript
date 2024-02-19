7b2974b706ddc91cb0464db14ec5b335f5aeb617
shihab20105 <shihabur10@gmail.com>
2024-02-19 20:54:04 +0000

    Discovered What Is Variable, Five Things You Need To Declare A Variable

---
+++ 17-3 What Is Variable, Five Things for Var/variable.js
@@ -0,0 +1,7 @@
+// vary + able = variable
+
+// To declare variable in JavaScript you need five things.
+// The var keyword was used in all JavaScript code from 1995 to 2015.
+var price = 10;
+var age = 17;
+var temperature = 20;
