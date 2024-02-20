39a2ac5171f943e533559905ff71c5c0e7907d56
shihab20105 <shihabur10@gmail.com>
2024-02-20 22:20:41 +0000

    Explored JavaScript Keyword, Variable Name Naming Convention

---
+++ 17-5 JS Keyword, Var Naming Convention/variable-name.js
@@ -0,0 +1,30 @@
+// Good Variable
+var price = 40;
+
+// You can not do this on JavaScript.
+vaR price = 40;
+Var price = 40;
+VAR price = 40
+
+// 1. Variable name can not be any keywords.
+var false = 95;
+var return = true;
+
+// 2. Variable name has to be in one word. No space.
+var my home address = "London Ilford"
+
+// 3. Variable name can not have quotation. 
+var "name" = "Tom Hanks";
+
+// 4. Variable name can not start with a number but can ends with a number. 
+var 95club = 1995;
+var club25 = 2025;  // you can do this .
+
+// 5. How to use long names.
+var user-name = "Salman Shah"; //Can not use dash.
+var user home address = "Redbridge Essex London";
+var userhomeaddress = "Redbridge Essex London";
+var user_home_address = "Redbridge Essex London"; //Snake Case
+var userHomeAddress = "Redbridge Essex London"; //Camel Case 
+var UserHomeAddress = "Redbridge Essex London"; //Pascal Case
+// We will use Camel Case.
