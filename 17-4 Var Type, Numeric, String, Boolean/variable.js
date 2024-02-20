f3412c553bb6f390625228d4645764ba77b297dc
shihab20105 <shihabur10@gmail.com>
2024-02-20 20:28:54 +0000

    Explored Variable Type, Numeric, String, Boolean

---
+++ 17-4 Var Type, Numeric, String, Boolean/variable.js
@@ -0,0 +1,20 @@
+// vary + able = variable
+
+// To declare variable in JavaScript you need five things.
+// The var keyword was used in all JavaScript code from 1995 to 2015.
+
+// numeric
+var price = 10;
+var age = 17;
+var temperature = 20;
+
+// string
+var person = "Hasan Ahmed" ;
+var location = "Red Bridge London" ;
+var special = "Delwar Hussain Saidi" ;
+var job = 'Working On Web Development' ;
+
+// boolean
+var serious = true;
+var isFullMarks = true;
+var isMarried = false;
