f9cd4c051dfe7643c5d83d7f5a63a0cf0204df87
shihab20105 <shihabur10@gmail.com>
2024-02-27 23:02:35 +0000

    Explored Simple Mathematical Operations In JavaScript

---
+++ 17-6 Simple Mathematical Operations JS/operation.js
@@ -0,0 +1,27 @@
+var onionPrice = 20;
+// console.log(onionPrice)
+var eggPrice = 10;
+// console.log(eggPrice)
+
+// Addition, Subtraction
+var totalPrice = onionPrice + eggPrice;
+
+var priceDifference = onionPrice - eggPrice;
+// console.log(priceDifference);
+
+// console.log(totalPrice);
+
+
+// console.log(onionPrice + eggPrice);
+
+// Multiplication 
+var orangePrice = 20;
+var quantity = 7;
+var totalCost = orangePrice * quantity;
+// console.log(totalCost);
+
+// Division 
+var money = 500;
+var player = 10;
+var eachPlayer = money / player;
+console.log(eachPlayer)
