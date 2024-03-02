23e7d2e5598cd82fbbdf15e163555e163f7e2152
shihab20105 <shihabur10@gmail.com>
2024-03-02 22:42:55 +0000

    Explored (Advanced) String Concatenation, Type Conversion ParseInt ParseFloat

---
+++ 17-8 (Adv) String Concatenation/conversion.js
@@ -0,0 +1,21 @@
+var price1 = 35;
+var price2 = 10;
+var totalPrice = price1 + price2;
+// console.log(totalPrice);
+
+var firstName = 'Jennie';
+var lastName = 'Kim';
+var fullName = firstName + lastName;
+// console.log(fullName);
+var firstName = 'Jennie';
+var lastName = 'Kim';
+var fullName = firstName +'x' +lastName;
+// console.log(fullName);
+var firstName = 'Jennie';
+var lastName = 'Kim';
+var fullName = firstName +'' +lastName;
+// console.log(fullName);
+var firstName = 'Jennie';
+var lastName = 'Kim';
+var fullName = firstName +' ' +lastName;
+console.log(fullName);
