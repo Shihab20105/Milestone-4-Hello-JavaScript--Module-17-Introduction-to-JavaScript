f0f7536b7d5ad08c85dd25a0df47d8adc308840a
shihab20105 <shihabur10@gmail.com>
2024-03-03 22:01:25 +0000

    Explored Variable Types, ToFixed, ParseFloat And Remainder

---
+++ 17-9 Variable Types, ToFixed, ParseFloat/remainder.js
@@ -0,0 +1,35 @@
+// var mangos = 7;
+// var person = 2;
+
+//   / This means Quotient(Vagfol)
+//  % This means Remainder(Vagshesh)
+
+
+// var mangos = 7;
+// var person = 2;
+// console.log(mangos/person);
+
+// var mangos = 10;
+// var person = 2;
+// console.log(mangos/person);
+
+// var mangos = 11;
+// var person = 2;
+// console.log(mangos/person);
+
+// var mangos = 11;
+// var person = 2;
+// console.log(mangos % person);
+
+// var mangos = 11;
+// var person = 3;
+// console.log(mangos % person);
+
+// var mangos = 19;
+// var person = 7;
+// console.log(mangos % person);
+
+var mangos = 19;
+var person = 7;
+var remainder = mangos % person;
+console.log(remainder);
---
+++ 17-9 Variable Types, ToFixed, ParseFloat/types.js
@@ -0,0 +1,61 @@
+var price = 100;
+// console.log(price);
+
+// console.log(typeof price);
+
+var price = '100';
+// console.log(typeof price)
+
+var isHappy = true;
+// console.log(typeof isHappy);
+
+var romantic;
+// console.log(typeof romantic);
+
+
+// * Advanced *
+
+// var num1 = 0.1;
+// var num2 = 0.2;
+// var sum = num1 + num2;
+// console.log(sum);
+
+// var num1 = 0.5;
+// var num2 = 0.2;
+// var sum = num1 + num2;
+// console.log(sum);
+
+// var num1 = 0.1;
+// var num2 = 0.3;
+// var sum = num1 + num2;
+// console.log(sum);
+
+// var num1 = 0.1;
+// var num2 = 0.2;
+// var sum = num1 + num2;
+// console.log(sum);
+
+// var num1 = 0.1;
+// var num2 = 0.2;
+// var sum = num1 + num2;
+// sum = sum.toFixed(2);
+// console.log(sum);
+
+// var num1 = 0.1;
+// var num2 = 0.2;
+// var sum = num1 + num2;
+// sum = sum.toFixed(4);
+// console.log(sum);
+
+// var num1 = 0.1;
+// var num2 = 0.2;
+// var sum = num1 + num2;
+// sum = sum.toFixed(1);
+// console.log(sum);
+
+var num1 = 0.1;
+var num2 = 0.2;
+var sum = num1 + num2;
+sum = sum.toFixed(1);
+sum = parseFloat(sum);
+console.log(sum);
