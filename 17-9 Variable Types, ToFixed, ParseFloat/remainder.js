290ecbb7935dc724102c1f64a617417ce99c5944
shihab20105 <shihabur10@gmail.com>
2024-05-04 01:02:53 +0100

    Discovered Variable Types, ToFixed, ParseFloat And Remainder

--- 17-9 Variable Types, ToFixed, ParseFloat/remainder.js
+++ 17-9 Variable Types, ToFixed, ParseFloat/remainder.js
@@ -1,35 +1,52 @@
-// var mangos = 7;
-// var person = 2;
+/*                      Remainder */
 
-//   / This means Quotient(Vagfol)
-//  % This means Remainder(Vagshesh)
+var mangos = 7;
+var person = 2;
+
+/*   / This means Quotient(Vagfol)
+
+ % This means Remainder(Vagshesh) */
 
 
-// var mangos = 7;
-// var person = 2;
-// console.log(mangos/person);
+var mangos = 7;
+var person = 2;
+console.log(mangos/person);
+/* Output:
+3.5 */
 
-// var mangos = 10;
-// var person = 2;
-// console.log(mangos/person);
+var mangos = 10;
+var person = 2;
+console.log(mangos/person);
+/* Output:
+5 */
 
-// var mangos = 11;
-// var person = 2;
-// console.log(mangos/person);
+var mangos = 11;
+var person = 2;
+console.log(mangos/person);
+/* Output:
+5.5 */
 
-// var mangos = 11;
-// var person = 2;
-// console.log(mangos % person);
+var mangos = 11;
+var person = 2;
+console.log(mangos % person);
+/* Output:
+1 */
 
-// var mangos = 11;
-// var person = 3;
-// console.log(mangos % person);
+var mangos = 11;
+var person = 3;
+console.log(mangos % person);
+/* Output:
+2 */
 
-// var mangos = 19;
-// var person = 7;
-// console.log(mangos % person);
+var mangos = 19;
+var person = 7;
+console.log(mangos % person);
+/* Output:
+5 */
 
 var mangos = 19;
 var person = 7;
 var remainder = mangos % person;
 console.log(remainder);
+/* Output:
+5 */
--- 17-9 Variable Types, ToFixed, ParseFloat/types.js
+++ 17-9 Variable Types, ToFixed, ParseFloat/types.js
@@ -1,57 +1,83 @@
-var price = 100;
-// console.log(price);
+/*                      Variable Types */
 
-// console.log(typeof price);
+var price = 100;
+console.log(price);
+/* Output:
+100 */
+
+console.log(typeof price);
+/* Output:
+number */
 
 var price = '100';
-// console.log(typeof price)
+console.log(typeof price);
+/* Output:
+string */
 
 var isHappy = true;
-// console.log(typeof isHappy);
+console.log(typeof isHappy);
+/* Output:
+boolean */
 
 var romantic;
-// console.log(typeof romantic);
+console.log(typeof romantic);
+/* Output:
+undefined */
 
 
-// * Advanced *
+/*                          Advanced  */
 
-// var num1 = 0.1;
-// var num2 = 0.2;
-// var sum = num1 + num2;
-// console.log(sum);
+var num1 = 0.1;
+var num2 = 0.2;
+var sum = num1 + num2;
+console.log(sum);
+/* Output:
+0.30000000000000004 */
 
-// var num1 = 0.5;
-// var num2 = 0.2;
-// var sum = num1 + num2;
-// console.log(sum);
+var num1 = 0.5;
+var num2 = 0.2;
+var sum = num1 + num2;
+console.log(sum);
+/* Output:
+0.7 */
 
-// var num1 = 0.1;
-// var num2 = 0.3;
-// var sum = num1 + num2;
-// console.log(sum);
+var num1 = 0.1;
+var num2 = 0.3;
+var sum = num1 + num2;
+console.log(sum);
+/* Output:
+0.4 */
 
-// var num1 = 0.1;
-// var num2 = 0.2;
-// var sum = num1 + num2;
-// console.log(sum);
+var num1 = 0.1;
+var num2 = 0.2;
+var sum = num1 + num2;
+console.log(sum);
+/* Output:
+0.30000000000000004 */
 
-// var num1 = 0.1;
-// var num2 = 0.2;
-// var sum = num1 + num2;
-// sum = sum.toFixed(2);
-// console.log(sum);
+var num1 = 0.1;
+var num2 = 0.2;
+var sum = num1 + num2;
+sum = sum.toFixed(2);
+console.log(sum);
+/* Output:
+0.30 */
 
-// var num1 = 0.1;
-// var num2 = 0.2;
-// var sum = num1 + num2;
-// sum = sum.toFixed(4);
-// console.log(sum);
+var num1 = 0.1;
+var num2 = 0.2;
+var sum = num1 + num2;
+sum = sum.toFixed(4);
+console.log(sum);
+/* Output:
+0.3000 */
 
-// var num1 = 0.1;
-// var num2 = 0.2;
-// var sum = num1 + num2;
-// sum = sum.toFixed(1);
-// console.log(sum);
+var num1 = 0.1;
+var num2 = 0.2;
+var sum = num1 + num2;
+sum = sum.toFixed(1);
+console.log(sum);
+/* Output:
+0.3 */
 
 var num1 = 0.1;
 var num2 = 0.2;
@@ -59,3 +85,5 @@ var sum = num1 + num2;
 sum = sum.toFixed(1);
 sum = parseFloat(sum);
 console.log(sum);
+/* Output:
+0.3 */
